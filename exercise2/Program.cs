2bb191b1afe0a52278188bc8f9ca4be60fe00ced
ren jastine <rnjstntmj@gmail.com>
2025-11-02 21:09:44 +0800

    Exercise 2 complete

---
+++ exercise2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Create a program that adds, substracts, multiplies, and divides two numbers.
+
+int num1 = 20, num2 = 15;
+
+int sum = num1 + num2;
+int difference = num1 - num2;
+int product = num1 * num2;
+double quotient = (double)num1 / num2;
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
+
