aff443da292e41319696c683c6b95766942e359a
ren jastine <rnjstntmj@gmail.com>
2025-11-02 20:56:27 +0800

    Exercise 1 complete

---
+++ exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Write a program that asks for your name and age, then print:
+// Hello [name], you are [age] years old.
+
+string name;
+int age;
+
+Console.Write("Enter your name: ");
+name = Console.ReadLine();
+Console.Write("Enter your age: ");
+age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hello {name}, you are {age} years old.");
+
+// I just put it so the console window doesn't close immediately
+Console.ReadKey();
+
