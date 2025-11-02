328f22df3d32dafb320c9a1211f56877d07af6ff
ren jastine <rnjstntmj@gmail.com>
2025-11-02 22:13:39 +0800

    Exercise 7 complete

---
+++ exercise7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Show the multiplication table of a number
+
+int number = 10;
+int multiplier = 1;
+
+for (int i = 1; i <= number; i++)
+{
+    for(int j = 1; j <= 10; j++)
+    {   
+        int product = j * multiplier;
+        Console.Write($"{product}\t");
+    }
+    multiplier++;
+    Console.WriteLine();
+}
