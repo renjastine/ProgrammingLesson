b717d721aa1cbac1397bf49778b9860ee65e5086
ren jastine <rnjstntmj@gmail.com>
2025-11-02 23:14:45 +0800

    Exercise 10 complete

---
+++ exercise10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Create a program that reverses a word (for example, "apple" becomes "elppa")
+
+string word = "apple";
+string reversedWord = "";
+
+for (int i = word.Length - 1; i >= 0; i--)
+{
+    reversedWord += word[i];
+}
+
+Console.WriteLine($"The reversed word of '{word}' is '{reversedWord}'.");
