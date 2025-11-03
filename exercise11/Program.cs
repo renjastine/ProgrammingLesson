df7893cf15651e2689c002eac995226e86e6ea84
ren jastine <rnjstntmj@gmail.com>
2025-11-03 15:12:01 +0800

    Exercise 11 complete

---
+++ exercise11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ask the user to enter 5 numbers, store them in an array, and show their average.
+Console.WriteLine("NOTE: No Space, Separated by Comma ONLY");
+Console.Write("Enter numbers: ");
+string input = Console.ReadLine();
+
+int[] numbers = Array.ConvertAll(input.Split(','), int.Parse);
+
+int sum = 0;
+foreach(int num in numbers)
+{
+    sum += num;
+}
+
+Console.WriteLine($"The average is: {(double)sum / numbers.Length}");
